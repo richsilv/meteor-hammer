a0204c0e49d89d509da41443f6d655690777e8d3
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2014-10-13 10:12:55 +0100

    updated to Hammer.js 2.0.4

--- package.js
+++ package.js
@@ -1,8 +1,14 @@
 Package.describe({
-  summary: 'A javascript library for multi-touch gestures'
+  "name": "richsilv:hammer",
+  "description": "JQuery Wrapper for the Hammer.js package",
+  "homepage": "https://github.com/noorderstorm/meteor-hammer",
+  "author": "Richard Venneman <richard@noorderstorm.com> (http://noorderstorm.com)",
+  "version": "0.1.4",
+  "git": "https://github.com/noorderstorm/meteor-hammer.git",
 });
 
 Package.on_use(function (api) {
   api.use('jquery', 'client');
+  api.use('quarterto:hammer@2.0.4')
   api.add_files(['jquery.hammer.js/jquery.hammer.js'], 'client');
 });
